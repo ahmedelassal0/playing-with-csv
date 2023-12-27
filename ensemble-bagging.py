e41439369fabe5f4c3837470d0032eefb7272909
DevAhmedKhalil <dev.akhalil01@gmail.com>
2023-12-27 02:49:37 +0200

    modified:   ensemble-bagging.py ak

--- ensemble-bagging.py
+++ ensemble-bagging.py
@@ -3,20 +3,21 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import BaggingRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score, mean_squared_log_error, median_absolute_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score, mean_squared_log_error, \
+    median_absolute_error, r2_score
 
 # Importing the 'clean_df' DataFrame from the 'config' module
 from config import clean_df
 
 # Extract relevant columns from the DataFrame
 data = clean_df.loc[
-    :,
-    ('price', 'name', 'host_id', 'room_type',
-     'minimum_nights', 'number_of_reviews',
-     'reviews_per_month',
-     'availability_365'
-     )
-]
+       :,
+       ('price', 'name', 'host_id', 'room_type',
+        'minimum_nights', 'number_of_reviews',
+        'reviews_per_month',
+        'availability_365'
+        )
+       ]
 
 # Create a DataFrame using the extracted data
 df = pd.DataFrame(data)
@@ -42,7 +43,6 @@ bagging_regressor.fit(x_train, y_train)
 
 y_pred = bagging_regressor.predict(x_test)
 
-
 mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 evs = explained_variance_score(y_test, y_pred)
@@ -52,16 +52,25 @@ r2 = r2_score(y_test, y_pred)
 
 # Print the metrics
 print(f'Mean Absolute Error (MAE): {mae}')
+# Mean Absolute Error (MAE): Represents the average absolute difference between predicted and actual values.
+# A lower MAE indicates better accuracy.
+
 print(f'Mean Squared Error (MSE): {mse}')
+# Mean Squared Error (MSE): Represents the average squared difference between predicted and actual values.
+# Like MAE, a lower MSE indicates better accuracy.
+
 print(f'Explained Variance Score (EVS): {evs}')
+# Explained Variance Score (EVS): [0 -> 1] Measures the proportion of variance in the target variable that is explained by the model.
+# A score of 1.0 indicates a perfect prediction.
+
 print(f'Mean Squared Log Error (MSLE): {msle}')
+# Mean Squared Log Error (MSLE): Measures the mean of the logarithmic squared differences between predicted and actual values.
+# Useful when the target variable spans several orders of magnitude.
+
 print(f'Median Absolute Error: {medae}')
+# Median Absolute Error: Represents the median absolute difference between predicted and actual values.
+# Similar to MAE but less sensitive to outliers.
+
 print(f'R-squared (R2): {r2}')
-# Make predictions on the test set
-# Evaluate the performance of the bagging regressor using various metrics
-# Mean Absolute Error (MAE): Represents the average absolute difference between predicted and actual values
-# Mean Squared Error (MSE): Represents the average squared difference between predicted and actual values
-# Explained Variance Score (EVS): Measures the proportion of variance in the target variable that is explained by the model
-# Mean Squared Log Error (MSLE): Measures the mean of the logarithmic squared differences between predicted and actual values
-# Median Absolute Error: Represents the median absolute difference between predicted and actual values
-# R-squared (R2): Represents the proportion of variance in the dependent variable that is predictable from the independent variables
+# R-squared (R2): Represents the proportion of variance in the dependent variable (target) that is predictable from the independent variables (features).
+# A higher R2 indicates a better fit.
